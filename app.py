fa11421879130506f13025bf15bf811ed97fb020
blackwolf660 <root@vm828324.stark-industries.solutions>
2023-03-17 08:51:54 +0100

    remove useless stuff

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import os
 import requests
 import discord
 import datetime
-import mysql.connector
 import time
 
 from requests import get
@@ -16,14 +15,9 @@ load_dotenv()
 donation = os.environ.get('token') #You donation alert token
 webhook_da = os.environ.get('webhook')
 webhook = DiscordWebhook(webhook_da)
-host = os.environ.get('host')
-database = os.environ.get('database')
-user = os.environ.get('user')
-password = os.environ.get('password')
 
 print(f"Remote DA destination set to: {donation}")
 print(F"Webhook is set to {webhook_da}")
-print("Donationalerts script startup successfully!")
 
 sio = socketio.Client()
 
@@ -35,36 +29,9 @@ def on_connect():
 def on_message(data):
 	y = json.loads(data)
 
-	conn = mysql.connector.connect(
-	   user=user, password=password, host=host, database=database)
-
-	cursor = conn.cursor()
-
-	sql = "SELECT * FROM donate"
-
-	cursor.execute(sql)
-
-	result = cursor.fetchall();
-	print("Total number of rows in table: ", cursor.rowcount)
-
-	print("\nPrinting each row while session")
-	for row in result:
-		print("donater = ", row[0], )
-		print("summa = ", row[1])
-		print("currency  = ", row[2])
-		print("message  = ", row[3], "\n")
-
-		# insert donate to sql
-		sql_query = "INSERT INTO donate (donater, summa, currency, message) VALUES (%s, %s, %s, %s)"
-		val = (y['username'], y['amount'], y['currency'], y['message'])
-		cursor.execute(sql_query, val)
-		conn.commit()
-
-	conn.close()
-
 	embed = DiscordEmbed(title='_**New donation!**_', description=f"**{y['username']}** donated **{y['amount']} {y['currency']}** with message: **{y['message']}**", color='03b2f8')
 	embed.set_footer(text='https://www.donationalerts.com/r/hewwo_kitty69', icon_url=f'https://a.okayu.me/3')
 	webhook.add_embed(embed)
 	response = webhook.execute()
 
-sio.connect('wss://socket.donationalerts.ru:443',transports='websocket')
+sio.connect('wss://socket.donationalerts.ru:443', transports='websocket')
